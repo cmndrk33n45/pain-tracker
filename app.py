40bf7e1caaa4857180d67c891b7082839376681e
Matt Revell <matt.timothy.revell@gmail.com>
2026-04-26 20:50:51 -0400

    added template
    added template system and set up app to load database and to route to a dashboard file.

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 # test code for first run
 import os #maybe not needed?
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 db = SQLAlchemy(app)
+
 
 @app.route("/")
 def home():
-    return "Pain Tracker App is running"
+    return render_template("dashboard.html")
 
 app.run(debug=True)
