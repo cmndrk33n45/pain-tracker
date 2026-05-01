fa63eda697d3a520a64182016ca5e1c449916478
Matt Revell <matt.timothy.revell@gmail.com>
2026-05-01 00:57:13 -0400

    signin and login work

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 db = SQLAlchemy(app)
@@ -33,9 +34,29 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    # check if form was submitted, otherwise provide the site.
+    if request.method == "POST":
+        # make sure are feilds were filled
+        errors = {}
+        if not request.form.get("username"):
+            errors["username"] = False
+        if not request.form.get("email"):
+            errors["email"] = False
+        if not request.form.get("password"):
+            errors["password"] = False
+        
+        # if any of those feilds were no filled out, send back to the login route and highlight incorrect forms.
+        if errors: 
+            return render_template("login.html", errors=errors)
+        else:
+            i = 0
+    else:
+        return render_template("login.html")
 
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template("signup.html")
+    if request.method == "POST":
+        i = 0
+    else:
+        return render_template("signup.html")
